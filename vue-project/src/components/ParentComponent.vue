a96f18a71b3522baa1c85605f874ccfadb8d4c9b
asif.mics <asif.mics@gmail.com>
2023-09-06 11:34:11 +0600

    Basic component and passing props

---
+++ vue-project/src/components/ParentComponent.vue
@@ -0,0 +1,29 @@
+<script setup>
+import ChildComponent from "@/components/ChildComponent.vue";
+import {reactive, ref} from "vue";
+
+const personLists = reactive([
+    {
+        name: 'asif',
+        age: 30,
+        is_active: 1
+    },
+    {
+        name: 'ahmed',
+        age: 31,
+        is_active: 1
+    },
+    {
+        name: 'ali',
+        age: 33,
+        is_active: 0
+    }
+])
+
+const count = ref(0)
+</script>
+
+<template>
+    <ChildComponent title="Person list" :persons="personLists" :count="count"/>
+</template>
+
